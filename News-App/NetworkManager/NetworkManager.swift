b8cbfda5f64b14fd8816d9a03b21bc854867d8bf
Ivan Osipov <ivan.loft72@gmail.com>
2023-03-02 00:52:16 +0500

    added functionality NetworkManager

---
+++ News-App/NetworkManager/NetworkManager.swift
@@ -0,0 +1,42 @@
+//
+//  NetworkManager.swift
+//  News-App
+//
+//  Created by Иван Осипов on 01.03.2023.
+//
+
+import Foundation
+
+protocol NetworkManagerProtocol {
+    func fetchNews(completion: @escaping (Result<[News]?, Error>) -> Void)
+}
+
+class NetworkManager: NetworkManagerProtocol {
+    private let baseURL = "https://newsapi.org/v2/top-headlines"
+    private let apiKey = "3937105f648849759b9ba2da70801d45"
+    
+    func fetchNews(completion: @escaping (Result<[News]?, Error>) -> Void) {
+        let urlString = "\(baseURL)?country=ru&apiKey=\(apiKey)"
+        performRequest(with: urlString, completion: completion)
+    }
+    
+    private func performRequest(with urlString: String, completion: @escaping (Result<[News]?, Error>) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                print(error)
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let decodedData = try JSONDecoder().decode(JsonResponce.self, from: data)
+                completion(.success(decodedData.articles))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+}
